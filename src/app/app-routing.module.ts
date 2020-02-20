0a5f6d11a70b6ca011547b171264535504846a40
WilliamMelgoza <wmelgoza@protonmail.com>
2020-02-19 21:16:59 -0600

    Implemented basic routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,21 @@
-import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import { NgModule }             from '@angular/core';
 
+// 1. Routing Libraries
+import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+// 2. Import the UserComponent
+import { UsersComponent }   from './users/users.component';
+
+// 3. Declare your routes
+const routes: Routes = [
+  // 4. The default route
+  { path: '', redirectTo: '/users', pathMatch: 'full' },
+  // 5. Map /users to the UsersComponent
+  { path: 'users', component: UsersComponent }
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  imports: [ RouterModule.forRoot(routes) ],
+  exports: [ RouterModule ]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
